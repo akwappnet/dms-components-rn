bcef02c6c7ba11d76805974e058bfc482612c674
Akshay <akshay@wappnet.com>
2024-06-05 20:40:22 +0530

    clean project

--- src/Components/JobHeader2.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export function JobHeader2() {
-  return (
-    <View style={{ flex: 1, backgroundColor: "green" }}>
-      <Text>DASWDFAW</Text>
-    </View>
-  );
-}
--- src/Utils/VersionUtils.js
+++
@@ -1,9 +0,0 @@
-import DeviceInfo from "react-native-device-info";
-
-const VersionUtils = {
-  getVersionNumber() {
-    return DeviceInfo.getVersion();
-  },
-};
-
-export default VersionUtils;
